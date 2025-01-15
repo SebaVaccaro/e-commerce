5503c19e77d08599f046224982883f758b32ebcb
SebaVaccaro <soy.vaccaro@gmail.com>
2025-01-15 15:47:08 -0300

    change sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,26 +3,26 @@ import { useState } from "react"
 import { CategoryButton } from "../buttons/header/category/CategoryButton"
 import "./sidebar.css"
 
+
 export const Sidebar = () => {
     const [state, setState] = useState<boolean>(false)
     return (
         <div className="sidebar">
-            <div onClick={()=>setState(!state)}>
-                <CategoryButton />
-            </div>
             {
-                state && (
+                state? (
                     <div className="sidebar-on">
-                        <div className="sidebar-category">
-
+                        <div className="sidebar-category" >
                         </div>
-                        <div className="sidebar-back">
-
+                        <div className="sidebar-back" onClick={()=>setState(!state)}>
                         </div>
+                    </div>
+                    
+                ): (
+                    <div onClick={()=>setState(!state)}>
+                        <CategoryButton />
                     </div>
                 )
             }
-
         </div>
     )
 }
