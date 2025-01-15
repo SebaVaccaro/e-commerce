8c0052075f3198a3c779859202a9a9caf70739ec
SebaVaccaro <soy.vaccaro@gmail.com>
2025-01-15 15:10:38 -0300

    add sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,28 @@
 
+import { useState } from "react"
 import { CategoryButton } from "../buttons/header/category/CategoryButton"
 import "./sidebar.css"
 
 export const Sidebar = () => {
+    const [state, setState] = useState<boolean>(false)
     return (
         <div className="sidebar">
-            <CategoryButton/>
+            <div onClick={()=>setState(!state)}>
+                <CategoryButton />
+            </div>
+            {
+                state && (
+                    <div className="sidebar-on">
+                        <div className="sidebar-category">
+
+                        </div>
+                        <div className="sidebar-back">
+
+                        </div>
+                    </div>
+                )
+            }
+
         </div>
     )
 }
