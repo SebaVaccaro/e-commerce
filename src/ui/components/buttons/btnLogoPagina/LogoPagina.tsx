1e8a809f6620fdc131820ac4fd9723de48ac2a71
SebaVaccaro <soy.vaccaro@gmail.com>
2025-01-18 20:37:04 -0300

    change header css

--- src/ui/components/buttons/btnLogoPagina/LogoPagina.tsx
+++ src/ui/components/buttons/btnLogoPagina/LogoPagina.tsx
@@ -3,7 +3,10 @@ import imgLogo from "../../../../assets/gamer-arcade-svgrepo-com.svg"
 export const LogoPagina = () => {
     return (
         <div className="logo-pagina">
-            <span className="span-logo-pagina">VaccaroStore</span>
+            <div className="span-container-logo-pagina">
+                <span className="span-logo-pagina">Vaccaro</span>
+                <span className="span-logo-pagina">Store</span>
+            </div>
             <img className="img-logo-pagina" src={imgLogo} alt="logo de la pagina" />
         </div>
     )
