eab0b67b1307d97d15f63adb04981a188740e3a5
SebaVaccaro <soy.vaccaro@gmail.com>
2025-02-07 14:38:33 -0300

    repair login/register

--- src/hooks/user/useRegister.tsx
+++ src/hooks/user/useRegister.tsx
@@ -10,23 +10,22 @@ type UserRegisterType = {
 export const useRegister = () => {
     const [dataRegister, setDataRegister] = useState<UserRegisterType | null>(null)
     const [error, setError] = useState<string | null>(null)
-    const [registerStatus , setRegisterStatus] = useState<boolean>(false)
+    const [registerStatus, setRegisterStatus] = useState<boolean>(false)
     const userServices = new UserService()
 
     useEffect(() => {
-        const registerFunction = async (data:{
+        const registerFunction = async (data: {
             username: string,
             email: string,
             password: string,
-        })=> {
+        }) => {
             const res = await userServices.register(data.username, data.password, data.email)
-            if(!res.success)setError(res.error)
-            console.log(res)
-            setRegisterStatus(res.success)
-}
-if (dataRegister) {
-    registerFunction(dataRegister)
-}
+            if (typeof res === "string")return setError(res)
+            setRegisterStatus(true)
+        }
+        if (dataRegister) {
+            registerFunction(dataRegister)
+        }
     }, [dataRegister])
-return { setDataRegister, error, registerStatus}
+    return { setDataRegister, error, registerStatus }
 }
