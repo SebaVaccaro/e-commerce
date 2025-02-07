9b357d92087c901ef0fb94101c2e75ac2bed5318
SebaVaccaro <soy.vaccaro@gmail.com>
2025-02-07 16:01:50 -0300

    change homepage

--- src/ui/pages/home/HomePage.tsx
+++ src/ui/pages/home/HomePage.tsx
@@ -1,7 +1,33 @@
 import "./home-page.css"
+import zonaAmd from "../../../assets/zona-amd.jpg"
+import zonaNvidea from "../../../assets/zona-nvidia.jpg"
+import equiposAmd from "../../../assets/equipos-amd.jpg"
+import equiposIntel from "../../../assets/equipos-armados-intel.jpg"
+import { Link } from "react-router-dom"
+
 export const HomePage = () =>{
     return(
         <div className="home-page">
+            <Link to='/componentes/tarjetas-graficas/amd'>
+            <div className="home-page-image-body">
+                <img className="home-page-img" src={zonaAmd} alt="imagen" />
+            </div>
+            </Link>
+            <Link to='/componentes/tarjetas-graficas/nvidia'>
+            <div className="home-page-image-body">
+                <img className="home-page-img" src={zonaNvidea} alt="imagen" />
+            </div>
+            </Link>
+            <Link to='/componentes/equipos'>
+            <div className="home-page-image-body">
+                <img className="home-page-img" src={equiposAmd} alt="imagen" />
+            </div>
+            </Link>
+            <Link to='/componentes/equipos'>
+            <div className="home-page-image-body">
+                <img className="home-page-img" src={equiposIntel} alt="imagen" />
+            </div>
+            </Link>
         </div>
     )
 }
